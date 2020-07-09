90ac2ac588ce91ac7ed01cf0e8ba18c5482fed00
Chester Black <chesterblack@wireless-n.home>
2020-07-09 22:27:34 +0100

    switched over to php

--- index.php
+++ index.php
@@ -1,11 +1,71 @@
 <?php
-
+    echo "<pre>";
     error_reporting(E_ALL);
-    echo "hello world";
+
+    function makeRequest($method, $url, $data, $headers)
+    {
+        try {
+            $options = array(
+                'http' => array(
+                    'header'  => $headers,
+                    'method'  => $method,
+                    'content' => $data ? http_build_query($data) : null
+                )
+            );
+            $context  = stream_context_create($options);
+            $result = file_get_contents($url, false, $context);
+            
+            return $result;
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
+
+    function getToken()
+    {
+        $requestBody = [
+            "grant_type" => "authorization_code",
+            "code" => $_GET['code'],
+            "redirect_uri" => "http://localhost:5907",
+            "client_id" => "1a0e4dc230e3429d9ad538490df3d3f0",
+            "client_secret" => "5968a94c7a3149ad9c56144af43fd842"
+        ];
+
+        $headers = "Content-type: application/x-www-form-urlencoded\r\n";
+
+        $response = json_decode(makeRequest(
+            "POST", 
+            "https://accounts.spotify.com/api/token", 
+            $requestBody, 
+            $headers
+        ));
+
+        $token = $response->access_token;
+
+        return $token;
+    }
+
+    function handlePlaylists()
+    {
+        $authToken = getToken();
+        $headers = "Accept: application/json\r\n";
+        $headers .= "Content-Type: application/json\r\n";
+        $headers .= "Authorization: Bearer ".$authToken."\r\n";
+        
+        $playlists = json_decode(makeRequest(
+            "GET", 
+            "https://api.spotify.com/v1/users/1114234527/playlists",
+            null,
+            $headers
+        ));
+        var_dump($playlists);
+    }
+
+    handlePlaylists();
 
 ?>
 
-<script>
+<!-- <script>
     function ajax(method, url, callback, data, auth) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -52,4 +112,4 @@
     }
     
     window.onload = getToken(handlePlaylists);
-</script>
+</script> -->
